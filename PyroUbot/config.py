5d0f48f712b1355b6cf31b53f0c55e45abebded7
S1teg4a <rafaszmotenzy@gmail.com>
2025-06-02 00:57:50 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -5,7 +5,7 @@ load_dotenv(".env")
 
 MAX_BOT = int(os.getenv("MAX_BOT", "200"))
 
-DEVS = list(map(int, os.getenv("DEVS", "6728786795").split()))
+DEVS = list(map(int, os.getenv("DEVS", "5716598718").split()))
 
 API_ID = int(os.getenv("API_ID", "22550997"))
 
